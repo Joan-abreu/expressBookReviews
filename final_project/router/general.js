533c8b783e68344a3992ad3b551afabd4344210c
Joan-abreu <joan.abreu2007@gmail.com>
2023-12-18 17:07:36 -0500

    Final code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 // Register a new user
 public_users.post("/register", (req, res) => {
     // Extract username and password from the request body
@@ -25,7 +24,6 @@ public_users.post("/register", (req, res) => {
   
     return res.status(201).json({ message: 'User registered successfully' });
   });
-
 
 // Function to get the list of books using async-await with Axios
 const getBookListAsync = async () => {
@@ -50,8 +48,11 @@ public_users.get('/', async (req, res) => {
             reviews: book.reviews
         }));
 
-        // Send the formatted book list as a JSON response
-        res.status(200).json({ books: formattedBookList });
+        // Use JSON.stringify to format the JSON response with indentation
+        const formattedBookDetails = JSON.stringify({ books: formattedBookList }, null, 2);
+    
+        // Send the formatted book details as a JSON response
+        return res.status(200).send(formattedBookDetails);
 
     } catch (error) {
         // Handle errors
@@ -61,101 +62,100 @@ public_users.get('/', async (req, res) => {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-    // Retrieve the ISBN from the request parameters
-    const requestedISBN = req.params.isbn;
-  
-    // Find the book with the specified ISBN in the books object
-    const book = Object.values(books).find(book => book.isbn === requestedISBN);
-  
-    if (!book) {
-      // If the book is not found, return a 404 Not Found response
-      return res.status(404).json({ message: 'Book not found' });
+public_users.get('/isbn/:isbn', async (req, res) => {
+    try {
+        // Retrieve the ISBN from the request parameters
+        const requestedISBN = req.params.isbn;
+
+        // Call the asynchronous function to get the bookList
+        const bookList = await getBookListAsync();
+
+        // Convert the books object to an array of objects
+        const booksArray = Object.values(bookList);
+
+        // Find the book in the bookList array with the specified ISBN
+        const book = booksArray.find(book => book.isbn === requestedISBN);
+
+        if (!book) {
+            // If the book is not found, return a 404 Not Found response
+            return res.status(404).json({ message: 'Book not found' });
+        }
+
+        // Customize the book details you want to include in the response
+        const formattedBookDetails = JSON.stringify({ book: book }, null, 2);
+
+        // Send the formatted book details as a JSON response
+        return res.status(200).send(formattedBookDetails);
+
+    } catch (error) {
+        // If an error occurs, handle it and send an appropriate response
+        return res.status(error.statusCode || 500).json({ message: error.message || 'Internal Server Error' });
     }
-  
-    // Customize the book details you want to include in the response
-    const bookDetails = {
-      title: book.title,
-      author: book.author,
-      isbn: book.isbn,
-    };
-  
-    // Use JSON.stringify to format the JSON response with indentation
-    const formattedBookDetails = JSON.stringify({ book: bookDetails }, null, 2);
-  
-    // Send the formatted book details as a JSON response
-    return res.status(200).send(formattedBookDetails);
-  });
+});
   
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-    // Retrieve the author from the request parameters
-    const requestedAuthor = req.params.author;
-  
-    // Get all keys (ISBNs) for the 'books' object
-    const bookISBNs = Object.keys(books);
-  
-    // Array to store books with matching author
-    const matchingBooks = [];
-  
-    // Iterate through the 'books' array & check if the author matches
-    bookISBNs.forEach(isbn => {
-      const book = books[isbn];
-      if (book.author === requestedAuthor) {
-        // Customize the book details you want to include in the response
-        const bookDetails = {
-          title: book.title,
-          author: book.author,
-          isbn: book.isbn,
-        };
-        matchingBooks.push(bookDetails);
-      }
-    });
-  
-    if (matchingBooks.length === 0) {
-      // If no matching books are found, return a 404 Not Found response
-      return res.status(404).json({ message: 'Books by the author not found' });
+public_users.get('/author/:author', async (req, res) => {
+    try {
+        // Retrieve the author from the request parameters
+        const requestedAuthor = req.params.author;
+
+        // Call the asynchronous function to get the bookList
+        const bookList = await getBookListAsync();
+
+        // Convert the books object to an array of objects
+        const booksArray = Object.values(bookList);
+
+        // Filter the booksArray to get books by the specified author
+        const matchingBooks = booksArray.filter(book => book.author === requestedAuthor);
+
+        if (matchingBooks.length === 0) {
+            // If no matching books are found, return a 404 Not Found response
+            return res.status(404).json({ message: 'Books by the author not found' });
+        }
+
+        // Use JSON.stringify to format the JSON response with indentation
+        const formattedMatchingBooks = JSON.stringify({ books: matchingBooks }, null, 2);
+
+        // Send the formatted matching books as a JSON response
+        return res.status(200).send(formattedMatchingBooks);
+
+    } catch (error) {
+        // If an error occurs, handle it and send an appropriate response
+        return res.status(error.statusCode || 500).json({ message: error.message || 'Internal Server Error' });
     }
-  
-    // Use JSON.stringify to format the JSON response with indentation
-    const formattedMatchingBooks = JSON.stringify({ books: matchingBooks }, null, 2);
-  
-    // Send the formatted matching books as a JSON response
-    return res.status(200).send(formattedMatchingBooks);
-  });
+});
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
-    // Retrieve the title from the request parameters
-    const requestedTitle = req.params.title;
-  
-    // Array to store books with matching title
-    const matchingBooks = [];
-  
-    // Iterate through the 'books' object & check if the title matches
-    Object.values(books).forEach(book => {
-      if (book.title.toLowerCase().includes(requestedTitle.toLowerCase())) {
-        // Customize the book details you want to include in the response
-        const bookDetails = {
-          title: book.title,
-          author: book.author,
-          isbn: book.isbn,
-        };
-        matchingBooks.push(bookDetails);
-      }
-    });
-  
-    if (matchingBooks.length === 0) {
-      // If no matching books are found, return a 404 Not Found response
-      return res.status(404).json({ message: 'Books with the title not found' });
+public_users.get('/title/:title', async (req, res) => {
+    try {
+        // Retrieve the title from the request parameters
+        const requestedTitle = req.params.title;
+
+        // Call the asynchronous function to get the bookList
+        const bookList = await getBookListAsync();
+
+        // Convert the books object to an array of objects
+        const booksArray = Object.values(bookList);
+
+        // Filter the booksArray to get books by the specified title
+        const matchingBooks = booksArray.filter(book => book.title.toLowerCase().includes(requestedTitle.toLowerCase()));
+
+        if (matchingBooks.length === 0) {
+            // If no matching books are found, return a 404 Not Found response
+            return res.status(404).json({ message: 'Books with the title not found' });
+        }
+
+        // Use JSON.stringify to format the JSON response with indentation
+        const formattedMatchingBooks = JSON.stringify({ books: matchingBooks }, null, 2);
+
+        // Send the formatted matching books as a JSON response
+        return res.status(200).send(formattedMatchingBooks);
+        
+    } catch (error) {
+        // If an error occurs, handle it and send an appropriate response
+        return res.status(error.statusCode || 500).json({ message: error.message || 'Internal Server Error' });
     }
-  
-    // Use JSON.stringify to format the JSON response with indentation
-    const formattedMatchingBooks = JSON.stringify({ books: matchingBooks }, null, 2);
-  
-    // Send the formatted matching books as a JSON response
-    return res.status(200).send(formattedMatchingBooks);
-  });
+});
 
 // Get book review
 public_users.get('/review/:isbn', function (req, res) {
@@ -177,6 +177,6 @@ public_users.get('/review/:isbn', function (req, res) {
   
     // Send the formatted reviews as a JSON response
     return res.status(200).send(formattedReviews);
-  });
+});
 
 module.exports.general = public_users;
